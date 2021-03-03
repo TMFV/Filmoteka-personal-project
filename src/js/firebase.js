16295ab8505cd2253079673365e04059041ff477
TMFV <68297974+TMFV@users.noreply.github.com>
2021-03-03 21:49:56 +0200

    webpack
    start webpack
    npm firebase install

---
+++ src/js/firebase.js
@@ -0,0 +1,32 @@
+//Registration for new User (Firebase)
+firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in 
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ..
+  });
+  //Log-in for user
+firebase.auth().signInWithEmailAndPassword(email, password)
+  .then((userCredential) => {
+    // Signed in
+    var user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    var errorCode = error.code;
+    var errorMessage = error.message;
+  });
+
+  //
+  //Sign-out for user
+  //
+firebase.auth().signOut().then(() => {
+  // Sign-out successful.
+}).catch((error) => {
+  // An error happened.
+});
